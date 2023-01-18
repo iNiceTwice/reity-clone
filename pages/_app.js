7c4dfceae20edf486f5b1f4f69a56f3693fe57ff
NiceTwice <onlynicetwice@gmail.com>
2023-01-18 19:07:24 -0300

    new host at .env

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import mapboxgl from 'mapbox-gl';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-console.log(process.env.NEXT_PUBLIC_MAPBOX_TOKEN)
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 function MyApp({ Component, pageProps }) {
