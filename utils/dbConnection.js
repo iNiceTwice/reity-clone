5b6b786434cafbf9eee3148a78bb584979211b6e
NiceTwice <onlynicetwice@gmail.com>
2023-01-18 20:47:48 -0300

    comment utils

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-
+//connecting to db
 const connectDB = () => {
     mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
         .then(data=>console.log("- Database Online -"))
