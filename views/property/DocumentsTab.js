e2b30ff466cc1b84177fa12118fd5cc45971d3d9
NiceTwice <onlynicetwice@gmail.com>
2023-01-19 21:16:28 -0300

    documents tab responsive fixes

--- views/property/DocumentsTab.js
+++ views/property/DocumentsTab.js
@@ -10,10 +10,13 @@ const DocumentsTab = ({ data }) => {
                         <Button>Ver documentos</Button>
                     </a>
                 </div>
-                <div className="my-6 flex justify-between items-center w-full">
+                <div className="gap-5 my-6 flex justify-between items-center w-full">
                     <p className="text-main">Dirección token</p>
-                    <a className="text-slate-800/90 hover:text-slate-800/40 hover:border-slate-800/40 border-b border-slate-800/80" href={ data.tokens.contractUrl } target="_blank" rel="noreferer">
+                    <a className="md:flex hidden text-slate-800/90 hover:text-slate-800/40 hover:border-slate-800/40 border-b border-slate-800/80" href={ data.tokens.contractUrl } target="_blank" rel="noreferer">
                     { data.tokens.contract }
+                    </a>
+                    <a className="flex md:hidden text-slate-800/90 hover:text-slate-800/40 hover:border-slate-800/40 border-b border-slate-800/80" href={ data.tokens.contractUrl } target="_blank" rel="noreferer">
+                    0xbF...3FDc
                     </a>
                 </div>
             </div>
